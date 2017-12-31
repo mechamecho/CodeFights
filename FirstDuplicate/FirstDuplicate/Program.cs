c3ff292a356be4baae800c0fea161cc77e6897a2
Livepurposefully <nafissa_92@hotmail.com>
2017-12-31 18:18:08 -0500

    Finish FirstDuplicate Challenge in C#

---
+++ FirstDuplicate/FirstDuplicate/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstDuplicate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        int firstDuplicate(int[] a)
+        {
+            HashSet<int> Set = new HashSet<int>();
+            // List<int> ArrayNums= new List<int>();
+            foreach (int number in a)
+            {
+                if (Set.Contains(number))
+                {
+                    return number;
+                }
+                Set.Add(number);
+
+            }
+            return -1;
+        }
+    }
+}
